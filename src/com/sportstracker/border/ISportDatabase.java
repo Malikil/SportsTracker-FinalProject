7bf8cde5a2e7352287d138a2b9ce4b1a23deaaa4
Malikil <eboy.coltt5@gmail.com>
2019-06-18 20:00:29 -0700

    Add hibernate files
    * JAR files
    * Basic DAO class
    * Match entity and annotations

--- src/com/sportstracker/border/ISportDatabase.java
+++ src/com/sportstracker/border/ISportDatabase.java
@@ -5,7 +5,17 @@ import com.sportstracker.entities.*;
 
 public interface ISportDatabase
 {
+	/**
+	 * Gets a list of all matches in the database
+	 * @return Returns a List<Match> containing all matches in the database
+	 */
 	public List<Match> getAllMatches();
 	public List<Player> getAllPlayers();
 	public List<Team> getAllTeams();
+	/**
+	 * Adds a single match to the database
+	 * @param match The match to add
+	 * @return Returns the id of the match that was added
+	 */
+	public int addMatch(Match match);
 }
--- src/com/sportstracker/entities/Match.java
+++ src/com/sportstracker/entities/Match.java
@@ -1,6 +1,65 @@
 package com.sportstracker.entities;
 
+import java.util.Date;
+//import java.util.ArrayList;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Match")
 public class Match
 {
-
+	@Id
+	@GeneratedValue
+	private int Id;
+	private Team winner;
+	private Team loser;
+	private int winnerScore;
+	private int loserScore;
+	private String location;
+	private Date time;
+	// Leaving yellow/red cards out for now
+	
+	public Team getWinner() {
+		return winner;
+	}
+	public void setWinner(Team winner) {
+		this.winner = winner;
+	}
+	public Team getLoser() {
+		return loser;
+	}
+	public void setLoser(Team loser) {
+		this.loser = loser;
+	}
+	public int getWinnerScore() {
+		return winnerScore;
+	}
+	public void setWinnerScore(int winnerScore) {
+		this.winnerScore = winnerScore;
+	}
+	public int getLoserScore() {
+		return loserScore;
+	}
+	public void setLoserScore(int loserScore) {
+		this.loserScore = loserScore;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public Date getTime() {
+		return time;
+	}
+	public void setTime(Date time) {
+		this.time = time;
+	}
+	public int getId() {
+		return Id;
+	}
+	public boolean isDraw()	{
+		return winnerScore == loserScore;
+	}
 }
